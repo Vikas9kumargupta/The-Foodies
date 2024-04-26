d91cedc321646b7a1923be01bebf53a989996886
Vikas Kumar Gupta <vikasgup074@gmail.com>
2024-04-27 02:15:21 +0530

    Home Fragment

--- app/src/main/java/com/example/myapplication/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/Fragment/HomeFragment.kt
@@ -6,11 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
+import com.denzcoskun.imageslider.models.SlideModel
 //import com.denzcoskun.imageslider.constants.ScaleTypes
 //import com.denzcoskun.imageslider.interfaces.ItemChangeListener
 //import com.denzcoskun.imageslider.interfaces.ItemClickListener
 //import com.denzcoskun.imageslider.models.SlideModel
 import com.example.myapplication.R
+import com.example.myapplication.adapter.PopularAdapter
 import com.example.myapplication.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -18,12 +23,8 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
         return binding.root
     }
@@ -31,16 +32,36 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        val imageList = ArrayList<SlideModel>()
-//        imageList.add(SlideModel(R.drawable.banner1, ScaleTypes.FIT))
-//        imageList.add(SlideModel(R.drawable.banner2, ScaleTypes.FIT))
-//        imageList.add(SlideModel(R.drawable.banner3, ScaleTypes.FIT))
-//
-//        val imageSlider = binding.imageSlider
-//        imageSlider.setImageList(imageList)
-//        imageSlider.setImageList(imageList, ScaleTypes.FIT)
+        val imageList = ArrayList<SlideModel>()
+        imageList.add(SlideModel(R.drawable.banner1, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner2, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner3, ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner4,ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner5,ScaleTypes.FIT))
 
-//
+        val imageSlider = binding.imageSlider
+        imageSlider.setImageList(imageList)
+        imageSlider.setImageList(imageList, ScaleTypes.FIT)
+
+        imageSlider.setItemClickListener(object : ItemClickListener{
+            override fun doubleClick(position: Int) {
+
+            }
+
+            override fun onItemSelected(position: Int) {
+                val itemPosition = imageList[position]
+                val itemMessage="Selected Image $position"
+                Toast.makeText(requireContext(),itemMessage,Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        val foodName = listOf("Veg Roll","Salad","Ice-Cream","Dal-Makhani","Pasta","Burger","Fruit-Salad")
+        val foodPrice = listOf("$6","$6","$10","$3","$8","$6","$8")
+        val foodImage = listOf(R.drawable.menu1,R.drawable.menu2,R.drawable.menu3,R.drawable.menu4, R.drawable.menu5,R.drawable.menu6,R.drawable.menu7)
+        val adapter = PopularAdapter(foodName,foodPrice,foodImage)
+        binding.popularRV.layoutManager = LinearLayoutManager(requireContext())
+        binding.popularRV.adapter = adapter
+
     }
 
 }
---
+++ app/src/main/java/com/example/myapplication/adapter/PopularAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.PopularItemBinding
+
+class PopularAdapter(private val items: List<String>,private val price: List<String>, private val image:List<Int>) : RecyclerView.Adapter<PopularAdapter.PopularViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularViewHolder {
+        return PopularViewHolder(PopularItemBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: PopularViewHolder, position: Int) {
+        val item = items[position]
+        val images = image[position]
+        val price = price[position]
+        holder.bind(item,images,price)
+    }
+
+    class PopularViewHolder (private val binding: PopularItemBinding) : RecyclerView.ViewHolder(binding.root){
+        private val imageView = binding.popularFoodImage
+        fun bind(item: String, images: Int, price : String) {
+            binding.foodNamePopular.text = item
+            binding.pricePopular.text = price
+            imageView.setImageResource(images)
+        }
+
+    }
+}
